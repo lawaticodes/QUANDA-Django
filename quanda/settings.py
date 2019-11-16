44deaeb44df815ac1724cff4fe1f70e5c2f047cc
tinalimbu <tinalawati@gmail.com>
2019-11-16 18:27:01 +0000

    Set up database on Postgres

--- quanda/settings.py
+++ quanda/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'quanda.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'quanda_db_1',
+        'USER': 'postgres',
+        'PASSWORD': 'FORpostgres1.',
+        'HOST': '127.0.0.1',
+        'PORT': '8001',
     }
 }
 
