9416b3b4954a981745287fa3b52cbe3918ffca21
tinalimbu <tinalawati@gmail.com>
2019-11-19 23:13:08 +0000

    Added forum specific models

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Question(models.Model):
+    text = models.CharField(max_length=2000)
+    datetime = models.DateTimeField(default=timezone.now)
+
+
+class Answer(models.Model):
+    question = models.ForeignKey("Question", on_delete=models.CASCADE)
+    text = models.CharField(max_length=2000)
+    datetime = models.DateTimeField(default=timezone.now)
+    chosen = models.BooleanField(default=False)
