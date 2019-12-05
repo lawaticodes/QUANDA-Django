5e8ada0601927a956afc6b10e96c963461c51c0f
tinalimbu <tinalawati@gmail.com>
2019-12-05 23:09:34 +0000

    Created shared app for shared logic

--- quanda/settings.py
+++ quanda/settings.py
@@ -32,6 +32,9 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'forum.apps.ForumConfig',
+    'bundle.apps.BundleConfig',
+    'release.apps.ReleaseConfig',
+    'shared.apps.SharedConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
