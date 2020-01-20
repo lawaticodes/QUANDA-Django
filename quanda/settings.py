9f27ed6d5f26c205a6f23bd582c5a290ebe43cfd
tinalimbu <tinalawati@gmail.com>
2020-01-20 20:21:28 +0000

    Added CORS whitelist

--- quanda/settings.py
+++ quanda/settings.py
@@ -41,11 +41,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -72,6 +74,10 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'quanda.wsgi.application'
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000"
+]
 
 
 # Database
