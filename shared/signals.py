751c52ba52de23bebc3fcbdb8b9f26f139430b6d
tinalimbu <tinalawati@gmail.com>
2020-03-02 23:51:19 +0000

    Added signal to send confirmation email

---
+++ shared/signals.py
@@ -0,0 +1,15 @@
+from django.core.mail import send_mail
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from shared.models import User
+
+
+@receiver(post_save, sender=User)
+def send_confirmation_email(sender, instance, created, **kwargs):
+    if created:
+        email_message = f"Hi {instance.username}, it's great that you want to be a part of Quanda. Please click " \
+                        f"this [INSERT LINK HERE] to complete your registration."
+
+        send_mail("Quanda registration confirmation", email_message, "tinalimbutest@gmail.com", [instance.email])
+        # TODO: Fix this.
