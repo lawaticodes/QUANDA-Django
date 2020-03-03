f3a3a47a51e47d0e22baa146cad55d9c916e36be
tinalimbu <tinalawati@gmail.com>
2020-03-03 22:47:34 +0000

    Used EmailMessage

--- shared/signals.py
+++ shared/signals.py
@@ -1,4 +1,4 @@
-from django.core.mail import send_mail
+from django.core.mail import EmailMessage
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -9,7 +9,10 @@ from quanda.settings import EMAIL_HOST
 @receiver(post_save, sender=User)
 def send_confirmation_email(sender, instance, created, **kwargs):
     if created:
-        email_message = f"Hi {instance.username}, it's great that you want to be a part of Quanda. Please click " \
-                        f"this [INSERT LINK HERE] to complete your registration."
-
-        send_mail("Quanda registration confirmation", email_message, EMAIL_HOST, [instance.email])
+        EmailMessage(
+            "Quanda registration confirmation",
+            f"Hi {instance.username}, it's great that you want to be a part of Quanda.\nPlease click "
+            f"[INSERT LINK HERE] to complete your registration.",
+            EMAIL_HOST,
+            [instance.email]
+        ).send()
